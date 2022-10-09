9e6953d23b1c27169873ae5057089bcd740ea316
Patrick <113420241+Ngception@users.noreply.github.com>
2022-10-08 20:09:03 -0400

    test: fix router-related errors

--- src/components/Review/List/ReviewList.test.tsx
+++ src/components/Review/List/ReviewList.test.tsx
@@ -1,20 +1,42 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { createReviews } from '../../../shared/handlers';
+import {
+  createReviews,
+  mockWindowLocation,
+  restoreWindowLocation
+} from '../../../shared/handlers';
 import { ReviewList } from './ReviewList';
+import MockRouter from '../../testing/MockRouter';
 
 describe('ReviewList', () => {
+  const originalWindow = global.window;
+
+  beforeAll(() => {
+    mockWindowLocation();
+  });
+
+  afterAll(() => {
+    restoreWindowLocation(originalWindow);
+  });
   test('renders ReviewList component with multiple items', () => {
     const reviews = createReviews();
 
-    render(<ReviewList reviews={reviews} />);
+    render(
+      <MockRouter route="/">
+        <ReviewList reviews={reviews} />
+      </MockRouter>
+    );
 
     expect(screen.getByTestId('review-list')).toBeInTheDocument();
     expect(screen.getAllByTestId('review-item').length).toEqual(reviews.length);
   });
 
   it('renders ReviewList component with no items', () => {
-    render(<ReviewList reviews={[]} />);
+    render(
+      <MockRouter route="/">
+        <ReviewList reviews={[]} />
+      </MockRouter>
+    );
 
     expect(screen.getByTestId('review-list')).toBeInTheDocument();
     expect(screen.queryAllByTestId('review-item').length).toEqual(0);
