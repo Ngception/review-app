115420358ca4b2c24e26dba9e85c89761ee73d57
Patrick <113420241+Ngception@users.noreply.github.com>
2022-10-09 14:48:32 -0400

    feat: add component to display response

---
+++ src/components/Response/Item/ResponseItem.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+import { Response } from '../../../shared/interfaces';
+
+interface ResponseItemProps {
+  response: Response;
+}
+
+export const ResponseItem: FC<ResponseItemProps> = (
+  props: ResponseItemProps
+) => {
+  return (
+    <div data-testid="response-item">
+      <h2>ResponseItem works!</h2>
+      <p>{props.response.author}</p>
+      <p>{props.response.content}</p>
+      <p>{props.response.published_at}</p>
+      <p>{props.response.review_id}</p>
+    </div>
+  );
+};
