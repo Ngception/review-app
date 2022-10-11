0f06c300daa5586b1d2d3fc2483a50568f32e267
Patrick <113420241+Ngception@users.noreply.github.com>
2022-10-10 20:25:16 -0400

    chore: rename directory

--- src/components/Review/Item/ReviewItem.tsx
+++ src/components/Review/Item/ReviewItem.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import { useResponse } from '../../../shared/hooks';
 import { Review } from '../../../shared/interfaces';
-import { Icon } from '../../../shared/ui';
+import { Icon } from '../../../shared/components';
 import styles from './ReviewItem.module.css';
 
 interface ReviewItemProps {
