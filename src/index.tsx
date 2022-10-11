d5254147dfab048702acf7aff07528eaf015cf32
Patrick <113420241+Ngception@users.noreply.github.com>
2022-10-10 20:12:37 -0400

    chore: add icons and purecss

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import App from './App';
+import 'purecss/build/pure.css';
+import './index.css';
+
+library.add(fas);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
