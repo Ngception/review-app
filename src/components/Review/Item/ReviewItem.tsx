ea0b1d6eec77b0053fc9230e2afde6b9f133ac3f
Patrick <113420241+Ngception@users.noreply.github.com>
2022-10-10 20:15:19 -0400

    refactor: style review item

--- src/components/Review/Item/ReviewItem.tsx
+++ src/components/Review/Item/ReviewItem.tsx
@@ -1,19 +1,55 @@
 import { FC } from 'react';
+import { useResponse } from '../../../shared/hooks';
 import { Review } from '../../../shared/interfaces';
+import { Icon } from '../../../shared/ui';
+import styles from './ReviewItem.module.css';
 
 interface ReviewItemProps {
   review: Review;
 }
 
 export const ReviewItem: FC<ReviewItemProps> = (props: ReviewItemProps) => {
+  const { state } = useResponse();
+  const formattedDate = new Date(props.review.published_at).toLocaleDateString(
+    'en-US',
+    {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit'
+    }
+  );
+
   return (
-    <div data-testid="review-item">
-      <h2 data-testid="review-place">{props.review.place}</h2>
-      <p data-testid="review-rating">{props.review.rating}</p>
-      <p data-testid="review-content">{props.review.content}</p>
-      <p data-testid="review-author">{props.review.author}</p>
-      <p data-testid="review-published-at">{props.review.published_at}</p>
-      <p data-testid="review-id">{props.review.id}</p>
+    <div data-testid="review-item" className={styles['review-item']}>
+      <div className={styles['review-header']}>
+        <h2 data-testid="review-place">{props.review.place}</h2>
+        <div className={styles['review-rating']}>
+          {Array.from(Array(props.review.rating).keys()).map((_i) => (
+            <Icon name={'star'} key={_i} />
+          ))}
+        </div>
+      </div>
+      <p data-testid="review-content" className={styles['review-content']}>
+        {props.review.content}
+      </p>
+      <div className={`${styles['review-footer']}`}>
+        <p data-testid="review-author">{props.review.author}</p>
+        <p
+          data-testid="review-published-at"
+          className={styles['review-published-at']}
+        >
+          {formattedDate}
+        </p>
+        {state.responses.some(
+          (response) => response.review_id === props.review.id
+        ) ? (
+          <div className={styles['responses-indicator-icon']}>
+            <Icon name={'comments'} />
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
     </div>
   );
 };
