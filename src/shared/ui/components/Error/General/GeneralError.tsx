f5d7c81c5f13e8ed031a65e78a9e7ab6fc3219cd
Patrick <113420241+Ngception@users.noreply.github.com>
2022-10-11 21:42:01 -0400

    feat: add error component

---
+++ src/shared/ui/components/Error/General/GeneralError.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+import styles from './GeneralError.module.css';
+
+interface GeneralErrorProps {}
+
+export const GeneralError: FC<GeneralErrorProps> = (
+  props: GeneralErrorProps
+) => {
+  return (
+    <div className={styles['error-container']}>
+      <div className={styles['error-content']}>
+        <h1 className="title">Oops...something went wrong.</h1>
+        <div role="alert">
+          <p className="subtitle">Please try again by refreshing the page.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
